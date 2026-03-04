beb5fd4e2ebba3e22716eb86d38f3aab6c89ca6d
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2026-03-04 11:35:56 +0100

    Updated radargram look

--- scripts/render_kroppbreen.py
+++ scripts/render_kroppbreen.py
@@ -5,11 +5,11 @@ import matplotlib.patheffects
 import os
 from pathlib import Path
 
-def normalize(data: np.ndarray, contrast: float = 1.):
+def normalize(data: np.ndarray, mask, contrast: float = 1.):
     """Normalize the data and convert to an unsigned 8 bit integer array."""
     data_abs = np.abs(data)
-    minval_abs, maxval_abs = np.percentile(np.abs(data_abs[50:]), [0.01, 99])
-    data_abs = np.clip(contrast * (data - minval_abs) / (maxval_abs - minval_abs + 1e-12), 0, 1)
+    minval_abs, maxval_abs = np.percentile(data_abs[mask], [5, 97.5])
+    data_abs = np.clip(contrast * (data_abs - minval_abs) / (maxval_abs - minval_abs + 1e-12), 0, 1)
 
     return (data_abs * 255).astype("uint8")
 
@@ -67,7 +67,7 @@ def main():
         data["elev"] = data["elev"].broadcast_like(data["data_topographically_corrected"])
         mask = (data["elev"] >= data["elevation"]) | ((data["elev"] + data["depth"].max().item()) <= data["elevation"])
 
-        arr = normalize(data.data_topographically_corrected)
+        arr = normalize(data.data_topographically_corrected.values, ~mask)
 
         extent=(0, data["distance"].max().item() / 1e3, data["elevation"].min().item() - data["depth"].max().item(), data["elevation"].max().item())
 
